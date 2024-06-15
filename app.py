98c022dbf6b51085c0f3a764ebf8d621d11dbe34
Defin-121140022 <118004022+Nachtt-2021@users.noreply.github.com>
2024-06-15 16:14:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,133 +13,143 @@ from nltk.corpus import wordnet
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Bidirectional, Dense, Dropout
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 import joblib
 
+# Function to preprocess text
+def text_preprocessing(text):
+    nltk.download('punkt', quiet=True)
+    nltk.download('wordnet', quiet=True)
+    nltk.download('averaged_perceptron_tagger', quiet=True)
+    nltk.download('stopwords', quiet=True)
+
+    stop_words = set(stopwords.words("english"))
+    punctuation = list(string.punctuation)
+    stop_words.update(punctuation)
+    lemmatizer = WordNetLemmatizer()
+
+    def get_wordnet_pos(word):
+        tag = nltk.pos_tag([word])[0][1][0].upper()
+        tag_dict = {"J": wordnet.ADJ, "N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV}
+        return tag_dict.get(tag, wordnet.NOUN)
+
+    text = text.lower()
+    text = re.sub(r"\\n", " ", text)
+    text = text.strip()
+    text = re.sub(r"http\S+", " ", text)
+    text = re.sub(r"www.\S+", " ", text)
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in tokens]
+    text = ' '.join(tokens)
+    return text
+
+# Load tokenizer
+tokenizer = joblib.load('tokenizer_file.pkl')
+max_features = 10000  # adjust based on your tokenizer configuration
+sent_length = 500  # adjust based on your tokenizer configuration
+
 # Function to load the model
 @st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('job_prediction.h5', custom_objects=custom_objects)
+def load_dl_model():
+    model = load_model('job_prediction.h5')  # Load your Keras model here
     return model
 
 # Streamlit App
-st.title('Job Posting Fraud Detection')
-st.write('Analisis untuk mendeteksi penipuan dalam posting pekerjaan.')
+def main():
+    st.title('Job Posting Fraud Detection')
+    st.write('Enter a job description to detect if it\'s potentially fake.')
 
-# Upload the job description
-job_description = st.text_area("Masukkan deskripsi pekerjaan di sini:")
+    # Input job description
+    job_description = st.text_area("Enter job description here:")
 
-if st.button('Prediksi'):
-    if job_description:
-        # Text Preprocessing Functions
-        nltk.download('punkt', quiet=True)
-        nltk.download('wordnet')
-        nltk.download('averaged_perceptron_tagger')
-        nltk.download('stopwords')
+    if st.button('Predict'):
+        if job_description:
+            # Preprocess the input
+            job_description_processed = text_preprocessing(job_description)
+            job_description_encoded = tokenizer.texts_to_sequences([job_description_processed])
+            job_description_padded = pad_sequences(job_description_encoded, padding='pre', maxlen=sent_length)
 
-        stop_words = set(stopwords.words("english"))
-        punctuation = list(string.punctuation)
-        stop_words.update(punctuation)
-        lemmatizer = WordNetLemmatizer()
+            # Load the deep learning model
+            with st.spinner("Loading Model...."):
+                model = load_dl_model()
 
-        def get_wordnet_pos(word):
-            tag = nltk.pos_tag([word])[0][1][0].upper()
-            tag_dict = {"J": wordnet.ADJ, "N": wordnet.NOUN, "V": wordnet.VERB, "R": wordnet.ADV}
-            return tag_dict.get(tag, wordnet.NOUN)
+            # Make prediction
+            prediction = model.predict(job_description_padded)[0][0]
 
-        def text_preprocessing(text):
-            text = text.lower()
-            text = re.sub(r"\\n", " ", text)
-            text = text.strip()
-            text = re.sub(r"http\S+", " ", text)
-            text = re.sub(r"www.\S+", " ", text)
-            tokens = word_tokenize(text)
-            tokens = [word for word in tokens if word not in stop_words]
-            tokens = [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in tokens]
-            text = ' '.join(tokens)
-            return text
-
-        job_description_processed = text_preprocessing(job_description)
-        tokenizer = joblib.load('tokenizer_file.pkl')
-        max_features = 10000
-        sent_length = 500  # Ensure you set this correctly based on your data preprocessing
-        job_description_encoded = tokenizer.texts_to_sequences([job_description_processed])
-        job_description_padded = pad_sequences(job_description_encoded, padding='pre', maxlen=sent_length)
-        
-        # Load the model
-        with st.spinner("Loading Model...."):
-            model = load_model()
-
-        # Make prediction
-        prediction = model.predict(job_description_padded)[0][0]
-        
-        if prediction >= 0.5:
-            st.error('Hasil Prediksi: Pekerjaan Palsu')
+            if prediction >= 0.5:
+                st.error('Prediction Result: Fake Job')
+            else:
+                st.success('Prediction Result: Genuine Job')
         else:
-            st.success('Hasil Prediksi: Pekerjaan Asli')
-    else:
-        st.warning('Silakan masukkan deskripsi pekerjaan terlebih dahulu.')
+            st.warning('Please enter a job description.')
 
-# Visualization
-# Load data
-df = pd.read_csv('fake_job_postings.csv')
-df['country'] = df['location'].apply(lambda x: x.split(',')[0])
-df.fillna(' ', inplace=True)
-df = df.drop_duplicates()
+    # Optional: Visualization or additional analysis
+    st.sidebar.subheader('Explore Data Insights')
+    st.sidebar.write('Choose an option to explore:')
+    option = st.sidebar.selectbox('Select an option',
+                                  ['Fraudulent vs Non-Fraudulent Distribution', 'Most Common Job Titles',
+                                   'Most Job Posted by Country', 'Most Required Experience'])
 
-st.subheader('Distribusi Fraudulent vs Non-Fraudulent')
-fig, ax = plt.subplots()
-sns.barplot(x=df['fraudulent'].value_counts().index, y=df['fraudulent'].value_counts().values, ax=ax)
-plt.xlabel('Fraudulent')
-plt.ylabel('Count')
-st.pyplot(fig)
+    # Load data for visualization
+    df = pd.read_csv('fake_job_postings.csv')
+    df['country'] = df['location'].apply(lambda x: x.split(',')[0])
+    df.fillna(' ', inplace=True)
+    df = df.drop_duplicates()
 
-st.subheader('Most Common Job Titles')
-fig, ax = plt.subplots()
-sns.barplot(x=df.title.value_counts()[:5].index, y=df.title.value_counts()[:5], ax=ax)
-plt.xlabel('Title')
-plt.ylabel('Count')
-plt.xticks(rotation=90)
-st.pyplot(fig)
+    if option == 'Fraudulent vs Non-Fraudulent Distribution':
+        st.subheader('Distribution of Fraudulent vs Non-Fraudulent Jobs')
+        fig, ax = plt.subplots()
+        sns.countplot(x='fraudulent', data=df, ax=ax)
+        plt.xlabel('Fraudulent')
+        plt.ylabel('Count')
+        st.pyplot(fig)
 
-country = dict(df.country.value_counts()[:11])
-if ' ' in country:
-    del country[' ']
+    elif option == 'Most Common Job Titles':
+        st.subheader('Most Common Job Titles')
+        fig, ax = plt.subplots()
+        sns.countplot(y='title', data=df, order=df['title'].value_counts().iloc[:5].index, ax=ax)
+        plt.xlabel('Count')
+        plt.ylabel('Job Title')
+        st.pyplot(fig)
 
-st.subheader('Most Job Posted by Country')
-fig, ax = plt.subplots()
-plt.bar(country.keys(), country.values())
-plt.xlabel('Countries')
-plt.ylabel('No. of Jobs')
-st.pyplot(fig)
+    elif option == 'Most Job Posted by Country':
+        st.subheader('Most Job Posted by Country')
+        fig, ax = plt.subplots()
+        sns.countplot(y='country', data=df, order=df['country'].value_counts().iloc[:10].index, ax=ax)
+        plt.xlabel('Count')
+        plt.ylabel('Country')
+        st.pyplot(fig)
 
-experience = dict(df.required_experience.value_counts())
-if ' ' in experience:
-    del experience[' ']
+    elif option == 'Most Required Experience':
+        st.subheader('Most Required Experience')
+        fig, ax = plt.subplots()
+        sns.countplot(y='required_experience', data=df, order=df['required_experience'].value_counts().index, ax=ax)
+        plt.xlabel('Count')
+        plt.ylabel('Experience Level')
+        st.pyplot(fig)
 
-st.subheader('Most Required Experience')
-fig, ax = plt.subplots()
-plt.bar(experience.keys(), experience.values())
-plt.xlabel('Experience')
-plt.ylabel('No. of Jobs')
-st.pyplot(fig)
+    st.sidebar.subheader('Word Cloud')
+    cloud_option = st.sidebar.selectbox('Select a Word Cloud',
+                                        ['Word Cloud for Fraudulent Job Postings', 'Word Cloud for Genuine Job Postings'])
 
-st.subheader('Word Cloud for Fraudulent Job Postings')
-fraudjobs_text_cleaned = df[df.fraudulent == 1]['combined_text_processed']
-wc = WordCloud(background_color="white", max_words=3000, width=1600, height=800, stopwords=STOPWORDS).generate(str(" ".join(fraudjobs_text_cleaned)))
-fig, ax = plt.subplots()
-plt.imshow(wc, interpolation='bilinear')
-plt.axis('off')
-st.pyplot(fig)
+    if cloud_option == 'Word Cloud for Fraudulent Job Postings':
+        st.subheader('Word Cloud for Fraudulent Job Postings')
+        fraudjobs_text_cleaned = df[df.fraudulent == 1]['description']
+        wc = WordCloud(background_color="white", max_words=300, stopwords=STOPWORDS).generate(' '.join(fraudjobs_text_cleaned))
+        plt.imshow(wc, interpolation='bilinear')
+        plt.axis('off')
+        st.pyplot()
 
-st.subheader('Word Cloud for Non-Fraudulent Job Postings')
-actualjobs_text_cleaned = df[df.fraudulent == 0]['combined_text_processed']
-wc = WordCloud(background_color="white", max_words=3000, width=1600, height=800, stopwords=STOPWORDS).generate(str(" ".join(actualjobs_text_cleaned)))
-fig, ax = plt.subplots()
-plt.imshow(wc, interpolation='bilinear')
-plt.axis('off')
-st.pyplot(fig)
+    elif cloud_option == 'Word Cloud for Genuine Job Postings':
+        st.subheader('Word Cloud for Genuine Job Postings')
+        actualjobs_text_cleaned = df[df.fraudulent == 0]['description']
+        wc = WordCloud(background_color="white", max_words=300, stopwords=STOPWORDS).generate(' '.join(actualjobs_text_cleaned))
+        plt.imshow(wc, interpolation='bilinear')
+        plt.axis('off')
+        st.pyplot()
+
+if __name__ == '__main__':
+    main()
